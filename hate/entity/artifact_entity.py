6c629dd322d210bd7540b9b32b96596d249c203c
santhoshmlops <santhoshmlops@gmail.com>
2023-05-14 02:27:14 +0530

    Final app file is created

--- hate/entity/artifact_entity.py
+++ hate/entity/artifact_entity.py
@@ -18,4 +18,8 @@ class ModelTrainerArtifacts:
 
 @dataclass
 class ModelEvaluationArtifacts:
-    is_model_accepted: bool    
+    is_model_accepted: bool   
+
+@dataclass
+class ModelPusherArtifacts:
+    bucket_name: str 
